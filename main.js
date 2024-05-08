96cd3feb4bef8da9f469abdff92b6ff0a6e6699d
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-05-08 22:50:43 +0800

    Proceed to check: basic declaration of different variable data types.

--- main.js
+++ main.js
@@ -3,6 +3,29 @@
 });
  */
 
+var stringNi = "This is a string!";
+var numberNi = 10;
+var isGymers = true;
+var fruits = ["apple", "banana", "orange"];
+var car = {
+    brand: "toyota",
+    model: 2024,
+    calculateSpeed: function () {
+        console.log("Speed is 150,000 km/s");
+    },
+};
+
+console.log(fruits[1]);
+console.log(car.model);
+console.log(car.calculateSpeed());
+
+var caculateSalary = function (salaryAmount, employeeName) {
+    console.log("Salary is minimum");
+};
+
+caculateSalary(5000, "Georgieee");
+caculateSalary(7000, "Jasonz");
+
 const onClickToggle = function () {
     console.log("onClick Toggled!");
     const containerElement = document.getElementById("container");
