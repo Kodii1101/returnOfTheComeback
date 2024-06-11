ad0f8cb3b76ae9489fc80d911c7d20d019736f5b
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-06-11 15:15:52 +0800

    proceed with body, all are without style, just basic css and html tags...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
-var nestedPs = document.querySelectorAll(".main-navbar .default-menu")
+/* var nestedPs = document.querySelectorAll(".main-navbar .default-menu")
 
 // Change the color of each <p> tag to red
 nestedPs.forEach(function (p) {
   p.style.background = "black"
   p.style.margin = "0px"
 })
+ */
